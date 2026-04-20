558f5ef0f410a6e63ea36b27966ba752024a1564
Sergiy Leshchak <sergiy.leshchak@group107.com>
2026-04-20 10:27:47 +0300

    Remove header component from MainLayout

--- src/app/layout/MainLayout.tsx
+++ src/app/layout/MainLayout.tsx
@@ -1,17 +1,10 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
 import styles from './MainLayout.module.scss';
 
 export function MainLayout() {
   return (
     <div className={styles.root}>
-      <header className={styles.header}>
-        <div className={styles.headerInner}>
-          <Link className={styles.brand} to="/">
-            RecruitGarden Tours
-          </Link>
-        </div>
-      </header>
       <main className={styles.main}>
         <Outlet />
       </main>
